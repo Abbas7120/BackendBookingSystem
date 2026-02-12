34d269f55e00ab2f6a8f5bc71d1a4c84abe999f3
Abbas <abbasfzd118@gmail.com>
2026-02-12 17:27:55 +0530

    init nodejs

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const {ServerConfig,Logger}=require('./config')
+const express=require('express')
+const apiRoutes=require('./routes')
+const logger = require('./config/logger-config')
+const app=express()
+
+app.use('/api',apiRoutes)
+
+
+app.listen(ServerConfig.PORT,()=>{
+    console.log(`Successfully started the server on ${ServerConfig.PORT}`)
+    logger.info("Successfully started the server",{})
+})
